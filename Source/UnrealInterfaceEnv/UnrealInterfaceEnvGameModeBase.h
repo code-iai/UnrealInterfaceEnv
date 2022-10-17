c2ada9907b76e40441f8b760b1bef95976284ca8
Sanic <Sanic@users.noreply.github.com>
2022-10-17 11:30:53 +0200

    initial commit

---
+++ Source/UnrealInterfaceEnv/UnrealInterfaceEnvGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealInterfaceEnvGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALINTERFACEENV_API AUnrealInterfaceEnvGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
